3523831ee45286cd22b6aeb0a0e1969356c64cc8
anil99 <anildhakedce99@gmail.com>
2017-10-03 02:34:44 +0530

    Add files via upload
    These are all session ,connection's

---
+++ session.php
@@ -0,0 +1,16 @@
+<?php
+   include('config.php');
+   session_start();
+   
+   $user_check = $_SESSION['login_user'];
+   
+   $ses_sql = mysqli_query($db,"select id from admin where id = '$user_check' ");
+   
+   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
+   
+   $login_session = $row['id'];
+   
+   if(!isset($_SESSION['login_user'])){
+      header("location:login.php");
+   }
+?>
